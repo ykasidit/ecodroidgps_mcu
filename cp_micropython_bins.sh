745f60bc182355dc5da07550a011e9657deed034
Kasidit Yusuf <ykasidit@gmail.com>
2022-11-24 12:31:07 +0700

    update micropython txpower lvl p7 as per new esp docs

---
+++ cp_micropython_bins.sh
@@ -0,0 +1,4 @@
+cd build-GENERIC
+cp ~/micropython/ports/esp32/build-GENERIC/micropython.bin .
+cp ~/micropython/ports/esp32/build-GENERIC/partition_table/partition-table.bin partition_table/partition-table.bin
+cp ~/micropython/ports/esp32/build-GENERIC/bootloader/bootloader.bin bootloader/bootloader.bin
